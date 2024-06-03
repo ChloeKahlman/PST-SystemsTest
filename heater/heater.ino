f9a9cdfc190a0bb27052430f1aa4c7692e77b741
Geoffrey Frankhuizen <31802157+GreenGearX@users.noreply.github.com>
2024-06-03 11:23:06 +0200

    Added code for heater.ino

--- heater/heater.ino
+++ heater/heater.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+// A short program that turns the heater on when switch 1 is on and off otherwise
+// Written by Geoffrey Frankhuizen
 
+// Wire connections:
+// D2 -> HTR
+// D5 -> SW1
+// GND -> GND (on the right)
+
+#define HEATER 2
+#define SWITCH 5
+
+void setup() {
+  pinMode(HEATER, OUTPUT);
+  pinMode(SWITCH, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if(digitalRead(SWITCH)){
+  digitalWrite(HEATER, HIGH);
+  } else {
+  digitalWrite(HEATER, LOW);
+  }
+  
+  delay(50);
 }
